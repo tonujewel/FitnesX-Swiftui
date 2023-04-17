b7ab53b97c4da9e02539bde1323accae9219a9db
Jewel rana <jewel.singularity@gmail.com>
2023-04-17 15:28:53 +0600

    login page design

--- fitnesx/screens/Auth/Register2.swift
+++ fitnesx/screens/Auth/Register2.swift
@@ -1,23 +1,19 @@
 //
-//  ContentView.swift
+//  Register2.swift
 //  fitnesx
 //
-//  Created by Singularity - Jewel Rana on 9/4/23.
+//  Created by Singularity - Jewel Rana on 17/4/23.
 //
 
 import SwiftUI
 
-struct ContentView: View {
-    
+struct Register2: View {
     @State var firstNameTextField:String = ""
-    @State private var  isOn = false
-    
-    
     var body: some View {
-        
         VStack(){
             
             Image(uiImage: UIImage(named: "vector1")!)
+                .padding(.bottom,10)
             
             Text("Let’s complete your profile")
                 .foregroundColor(.black)
@@ -27,7 +23,7 @@ struct ContentView: View {
             Text("It will help us to know more about you!")
                 .foregroundColor(Color(hex:0xFF7B6F72))
                 .font(.system(size: 12))
-       
+                .padding(.bottom,10)
             
             HStack{
                 Image(uiImage: UIImage(named: "2 User")!)
@@ -40,8 +36,8 @@ struct ContentView: View {
             .frame(maxWidth: .infinity)
             .padding()
             .background(RoundedRectangle(cornerRadius: 12).fill(Color(hex:0xffF7F8F8)))
+            .padding(.bottom,10)
             
-       
             
             HStack{
                 Image(uiImage: UIImage(named: "Calendar")!)
@@ -54,7 +50,7 @@ struct ContentView: View {
             .frame(maxWidth: .infinity)
             .padding()
             .background(RoundedRectangle(cornerRadius: 12).fill(Color(hex:0xffF7F8F8)))
-            
+            .padding(.bottom,10)
             HStack{
                 HStack{
                     Image(uiImage: UIImage(named: "weight-scale 1")!)
@@ -72,9 +68,12 @@ struct ContentView: View {
                     .bold()
                     .frame(width: 50,height: 50)
                     .foregroundColor(Color("white"))
-                    .background(LinearGradient(gradient: Gradient(colors: [Color(hex:0xffC58BF2),Color(hex:0xffEEA4CE)]), startPoint: .leading, endPoint: .trailing))
+                    .background(LinearGradient(gradient: Gradient(colors: [Color(hex:0xffEEA4CE),Color(hex:0xffC58BF2),]), startPoint: .leading, endPoint: .trailing))
                     .cornerRadius(15)
             }
+            .padding(.bottom,10)
+            
+            
             HStack{
                 HStack{
                     Image(uiImage: UIImage(named: "Swap")!)
@@ -92,18 +91,22 @@ struct ContentView: View {
                     .bold()
                     .frame(width: 50,height: 50)
                     .foregroundColor(Color("white"))
-                    .background(LinearGradient(gradient: Gradient(colors: [Color(hex:0xffC58BF2),Color(hex:0xffEEA4CE)]), startPoint: .leading, endPoint: .trailing))
+                    .background(LinearGradient(gradient: Gradient(colors: [Color(hex:0xffEEA4CE),Color(hex:0xffC58BF2),]), startPoint: .leading, endPoint: .trailing))
                     .cornerRadius(15)
             }
+            .padding(.bottom,20)
+            
+            
             Button{
                 print("clicked")
             }label: {
                 Text("Next")
                     .bold()
-                    .frame(width: 350,height: 50)
+                    .frame(maxWidth: .infinity,minHeight: 50)
                     .foregroundColor(Color("white"))
                     .background(LinearGradient(gradient: Gradient(colors: [Color(hex:0xff9DCEFF),Color(hex:0xff92A3FD)]), startPoint: .leading, endPoint: .trailing))
                     .clipShape(Capsule())
+                
             }
         }
         .background(Color(.white))
@@ -112,9 +115,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct Register2_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Register2()
     }
 }
-
